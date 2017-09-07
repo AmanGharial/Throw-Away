3132bf1af01895fc9d5c0c07aeee7862c0727120
AmanGharial <31664252+AmanGharial@users.noreply.github.com>
2017-09-07 14:46:21 -0600

    Freak out my friends :+1:

---
+++ Extension Methods/Quackers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quackers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Aman"; 
+            Console.WriteLine(name);
+            Console.WriteLine(name.Quack());
+
+        }
+    }
+
+    public static class Extensions //Note that this is static
+    {
+        public static string Quack(this string self)
+        {
+            return self + "(quack)";
+        }
+    }
+
+}
